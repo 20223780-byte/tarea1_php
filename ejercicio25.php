44b6b164aac40f0dd085aa6e6d07e882692ac8bc
20223780-byte <20223780@clases.edu.sv>
2026-05-20 17:22:17 -0600

    ejercicio 25
    es un formulario que solicita nombre, correo electrónico y ciudad del usuario.
    Los datos se envía a otro archivo PHP donde se muestra toda la
    información ingresada.

---
+++ ejercicio25.php
@@ -0,0 +1,11 @@
+<?php
+
+$nombre=$_POST['nombre'];
+$correo=$_POST['correo'];
+$ciudad=$_POST['ciudad'];
+
+echo "Nombre del usuario: ".$nombre."<br>";
+echo "Correo electronico: ".$correo."<br>";
+echo "Ciudad de origen: ".$ciudad;
+
+?>
